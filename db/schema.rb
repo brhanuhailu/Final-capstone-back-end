c458212724c75c521dd78c4b9d4afce747a076cd
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-09-21 16:58:19 +0300

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_131931) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_132559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_131931) do
     t.index ["user_id"], name: "index_houses_on_user_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "house_id", null: false
+    t.date "booking_date"
+    t.date "checkout_date"
+    t.decimal "total_charge"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["house_id"], name: "index_reservations_on_house_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
@@ -33,4 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_131931) do
   end
 
   add_foreign_key "houses", "users"
+  add_foreign_key "reservations", "houses"
+  add_foreign_key "reservations", "users"
 end
